85028d6decf53ac74a3e52c6f189441c24056b53
BlackPantific <blackpantific@gmail.com>
2022-10-22 16:18:02 +0300

    add dimens res

--- app/src/main/java/com/example/kotlin1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val savedNumber = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
         currentNumber = Preferences.getStoredNumber(applicationContext)
 
         prevButton = findViewById(R.id.prev_button)
