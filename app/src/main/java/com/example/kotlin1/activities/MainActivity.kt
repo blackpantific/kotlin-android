c5fdae469ca310337d7e254f70ae19a1beeea013
oleksii.vovchenko <oleksii.vovchenko@cigen.me>
2022-11-09 22:27:08 +0300

    first realization of DrawerLayout and NavigationView

--- app/src/main/java/com/example/kotlin1/activities/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1/activities/MainActivity.kt
@@ -2,50 +2,42 @@ package com.example.kotlin1.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.kotlin1.R
 import com.example.kotlin1.helpers.Preferences
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var prevButton: ImageButton
-    private lateinit var nextButton: ImageButton
-    private lateinit var numberOutput: TextView
-    private lateinit var task1Button: Button
-    private var currentNumber = 0
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        currentNumber = Preferences.getStoredNumber(applicationContext)
+        var drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        var navigationView = findViewById<NavigationView>(R.id.nav_view)
 
-        prevButton = findViewById(R.id.prev_button)
-        nextButton = findViewById(R.id.next_button)
-        numberOutput = findViewById(R.id.number_output)
-        task1Button = findViewById(R.id.task1_button)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.opened, R.string.closed)
 
-        numberOutput.text = currentNumber.toString()
+        drawerLayout.addDrawerListener(toggle)
 
-        prevButton.setOnClickListener {
-            if (currentNumber != 0) {
-                currentNumber--
-                Preferences.setStoredNumber(applicationContext, currentNumber)
-                numberOutput.text = currentNumber.toString()
-            }
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
         }
 
-        nextButton.setOnClickListener {
-            currentNumber++
-            Preferences.setStoredNumber(applicationContext, currentNumber)
-            numberOutput.text = currentNumber.toString()
-        }
-
-        task1Button.setOnClickListener {
-            val intent = FirstTaskActivity.newIntent(this@MainActivity)
-            startActivity(intent)
-        }
+        return super.onOptionsItemSelected(item)
     }
 }
