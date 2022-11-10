49bc097864e0758cf935aef0e25d3ac7d3a46062
oleksii.vovchenko <oleksii.vovchenko@cigen.me>
2022-11-10 14:48:33 +0300

    second realization of drawer layout

--- app/src/main/java/com/example/kotlin1/activities/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1/activities/MainActivity.kt
@@ -6,7 +6,10 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.kotlin1.R
 import com.example.kotlin1.helpers.Preferences
@@ -15,29 +18,47 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        var navigationView = findViewById<NavigationView>(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.opened, R.string.closed)
+//        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.opened, R.string.closed)
+//
+//        drawerLayout.addDrawerListener(toggle)
+//
+//        toggle.syncState()
+//
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.opened, R.string.closed)
 
         drawerLayout.addDrawerListener(toggle)
-
         toggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onBackPressed() {
 
-        if (toggle.onOptionsItemSelected(item)) {
-            return true
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
         }
-
-        return super.onOptionsItemSelected(item)
     }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//        if (toggle.onOptionsItemSelected(item)) {
+//            return true
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 }
